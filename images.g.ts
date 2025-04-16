bb4f5116199c512030a392e9cce4a061eb1c5be2
nayla-debug <n.otavaloanrango@Share.epsb.ca>
2025-04-16 14:35:49 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,138 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image3":
+            case "yuha":return img`
+. . . . . . . . . . . . . 
+. . f f f f f f f f . . . 
+. . f f f f f c f f . . . 
+. f f f f f c c f f f c . 
+. f f c f f f f f f f c . 
+. c c f f f 1 1 f f c c . 
+. f f f f 1 1 f f c c f . 
+. f f 6 f 1 1 f 6 f f f . 
+. f f 6 f 1 1 f 6 f f f . 
+. f 3 3 1 1 1 1 3 3 f f . 
+9 9 f 9 9 a 9 a 9 f 9 f . 
+1 1 f 9 9 a 9 a 9 f 1 f . 
+1 1 f 9 3 9 3 9 3 f 1 1 . 
+1 1 f 3 3 3 3 3 3 3 1 1 . 
+. . . 3 3 3 3 3 3 3 . . . 
+. . . f f . . f f . . . . 
+`;
+            case "image4":
+            case "stella":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "1#`=8TlM,M(!S!0OxjA3":
+            case "Jiwoo":return img`
+. . . . . . . . . . . . . . 
+. . f f f f f f f f . . . . 
+. . f f f f f f f b f f . . 
+1 f f f 3 f f f f f f f f . 
+. f 3 3 f f d d f f 3 3 f . 
+. f f f f d 1 1 d f f f f . 
+. f f f d f 1 1 f d f f f . 
+. f f f 6 f 1 1 f 6 f f f . 
+. f f 3 3 1 1 1 1 3 3 f f . 
+. . f f 1 1 1 1 1 1 f f . . 
+. . 1 1 1 9 1 1 9 1 1 1 . . 
+. . 1 1 6 9 9 9 9 6 1 1 . . 
+. . 1 1 9 6 9 9 6 9 1 1 . . 
+. . 1 1 9 6 6 6 6 9 1 1 . . 
+. . . 1 f f f f f f . . . . 
+. . . . f f . . f f . . . . 
+`;
+            case "image2":
+            case "Ana":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . 3 e e e e e 6 e . . . . 
+. . . e e e e e e e e 6 e . . . 
+. . . e e d e e e e e e e . . . 
+. . . e e d d d d f d e e . . . 
+. . e e e f f 1 1 f 6 e e e . . 
+. . e e 3 3 1 1 1 1 3 3 e e . . 
+. . e e e 1 1 1 1 1 6 e e e . . 
+. e e e e 4 1 1 1 1 4 e e e e . 
+. . . 1 1 4 4 5 5 5 4 1 1 . . . 
+. . . 1 1 5 4 4 4 5 4 1 1 . . . 
+. . 1 1 1 4 4 4 4 4 4 1 1 . . . 
+. . . 1 1 9 9 9 9 9 9 1 . . . . 
+. . . . . 9 9 9 9 9 9 . . . . . 
+. . . . . . f f . f f . . . . . 
+`;
+            case "image1":
+            case "Carmen":return img`
+. . . . . . . . . . . . . . 
+. f f f . . . . . . f f f . 
+. f f f a f f f f 8 f f f . 
+. f f 8 f f f f f f 8 f f . 
+. . 8 f f f f f f f f a . . 
+. . f f f f f f f f f f . . 
+. . f f f d d d d d f f . . 
+. . f f 6 f d d f 6 f f . . 
+. . f 3 6 f 1 1 f 6 3 f . . 
+. . f f 3 1 1 1 1 3 f f . . 
+. 1 1 f 9 9 9 9 9 9 3 1 . . 
+. 1 1 f 9 9 9 9 9 9 1 1 . . 
+. 1 1 9 9 9 9 9 9 9 1 1 . . 
+. 1 1 3 9 3 3 3 3 3 1 1 . . 
+. . . 9 3 3 3 3 3 3 . . . . 
+. . . . f f . . f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,6 @@
+let mySprite = sprites.create(assets.image`Jiwoo`, SpriteKind.Player)
+let mySprite2 = sprites.create(assets.image`Ana`, SpriteKind.Player)
+let mySprite3 = sprites.create(assets.image`yuha`, SpriteKind.Player)
+let mySprite4 = 0
+let mySprite5 = sprites.create(assets.image`stella`, SpriteKind.Player)
+mySprite3 = sprites.create(assets.image`Carmen`, SpriteKind.Player)
